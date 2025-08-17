db4192f8196c92875b363bdf4b27b15f9b8fd091
Emanuel Rivera <riveraaemanuel8418@gmail.com>
2025-08-16 21:50:06 -0500

    Se crea la entidad

---
+++ src/main/java/com/formacionntt/microservicios/app/examenes/models/entity/Examen.java
@@ -0,0 +1,58 @@
+package com.formacionntt.microservicios.app.examenes.models.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+@Table(name = "examenes")
+public class Examen {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name="create_at")
+	private Date createAt;
+	
+	@PrePersist
+	public void prePersist() {
+		this.createAt = new Date();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+	
+}
