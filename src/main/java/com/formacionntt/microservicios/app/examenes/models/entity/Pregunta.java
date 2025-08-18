6a95ce15b50cb83b50be3191e909473a978c27ca
Emanuel Rivera <riveraaemanuel8418@gmail.com>
2025-08-18 09:26:38 -0500

    se agregan atributos y metodos necesarios

--- src/main/java/com/formacionntt/microservicios/app/examenes/models/entity/Pregunta.java
+++ src/main/java/com/formacionntt/microservicios/app/examenes/models/entity/Pregunta.java
@@ -1,9 +1,14 @@
 package com.formacionntt.microservicios.app.examenes.models.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -15,6 +20,11 @@ public class Pregunta {
 	private Long id;
 	
 	private String texto;
+	
+	@JsonIgnoreProperties(value = {"preguntas"})
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "examen_id")
+	private Examen examen;
 
 	public Long getId() {
 		return id;
@@ -31,5 +41,14 @@ public class Pregunta {
 	public void setTexto(String texto) {
 		this.texto = texto;
 	}
+
+	public Examen getExamen() {
+		return examen;
+	}
+
+	public void setExamen(Examen examen) {
+		this.examen = examen;
+	}
+	
 	
 }
