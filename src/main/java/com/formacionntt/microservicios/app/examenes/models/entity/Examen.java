2c1a98d0d02565c301761372b06ae67505c3dbac
Emanuel Rivera <riveraaemanuel8418@gmail.com>
2025-08-18 09:26:27 -0500

    se agregan atributos y metodos necesarios

--- src/main/java/com/formacionntt/microservicios/app/examenes/models/entity/Examen.java
+++ src/main/java/com/formacionntt/microservicios/app/examenes/models/entity/Examen.java
@@ -1,12 +1,19 @@
 package com.formacionntt.microservicios.app.examenes.models.entity;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
@@ -26,9 +33,17 @@ public class Examen {
 	@Column(name="create_at")
 	private Date createAt;
 	
+	@JsonIgnoreProperties(value = {"examen"}, allowSetters = true)
+	@OneToMany(mappedBy = "examen", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Pregunta> preguntas;
+	
 	@PrePersist
 	public void prePersist() {
 		this.createAt = new Date();
+	}
+	
+	public Examen() {
+		this.preguntas = new ArrayList<>();
 	}
 
 	public Long getId() {
@@ -54,5 +69,43 @@ public class Examen {
 	public void setCreateAt(Date createAt) {
 		this.createAt = createAt;
 	}
+
+	public List<Pregunta> getPreguntas() {
+		return preguntas;
+	}
+
+	public void setPreguntas(List<Pregunta> preguntas) {
+		this.preguntas.clear();
+		preguntas.forEach(p -> {
+			this.addPregunta(p);
+		});
+	}
 	
+	public void addPregunta(Pregunta pregunta) {
+		this.preguntas.add(pregunta);
+		pregunta.setExamen(this);
+	}
+	
+	public void removePregunta(Pregunta pregunta) {
+		this.preguntas.remove(pregunta);
+		pregunta.setExamen(null);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		//este metodo es muy importante para eliminar una Pregunta
+		
+		//this hace referencia Pregunta y obj es el objeto que se pasa
+		if (this == obj) {
+			return true;
+		}
+		//sino es una instancia de Pregunta
+		if (!(obj instanceof Pregunta)) {
+			return false;
+		}
+		//casteamos
+		Pregunta p = (Pregunta) obj;
+		      //si el Id es nulo falla
+		return this.id != null && this.id.equals(p.getId());
+	}
 }
