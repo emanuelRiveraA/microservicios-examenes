8dae77acb97ceb456d8435f3cfa9aaccd6e5019b
Emanuel Rivera <riveraaemanuel8418@gmail.com>
2025-08-19 12:14:43 -0500

    se crea servicio examen

---
+++ src/main/java/com/formacionntt/microservicios/app/examenes/services/ExamenService.java
@@ -0,0 +1,8 @@
+package com.formacionntt.microservicios.app.examenes.services;
+
+import com.formacionntt.microservicios.app.examenes.models.entity.Examen;
+import com.formacionntt.microservicios.commons.services.CommonService;
+
+public interface ExamenService extends CommonService<Examen>{
+
+}
